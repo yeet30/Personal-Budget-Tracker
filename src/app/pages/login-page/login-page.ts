4c8b71926f303eaf5aa13ab0e26913967f3ee9af
Yiğit <ygtyigido0424@gmail.com>
2026-01-05 19:51:42 +0100

    Login and Registeration input validator has been added

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginForm,LoginSchema } from '../../form-validator';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
+  standalone: true,
 })
 export class LoginPage {
 
+  formData: LoginForm = {
+    email: '',
+    password: ''
+  }
+
+  loginAttempt(){
+    const result = LoginSchema.safeParse(this.formData)
+    
+    if(!result.success){
+      console.warn('Invalid payload blocked.', result.error)
+      return;
+    }
+    else {
+      console.log('Payload is being sent.');
+    }
+
+  }
 }
--- src/app/pages/register-page/register-page.ts
+++ src/app/pages/register-page/register-page.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RegisterationSchema, RegisterForm } from '../../form-validator';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.scss',
+  standalone: true,
 })
 export class RegisterPage {
 
+  formData : RegisterForm = {
+    username: '',
+    email: '',
+    password: '',
+    passwordAgain: ''
+  }
+  passwordPattern = /.*\d.*/;
+
+  RegisterAttempt(){
+    const result = RegisterationSchema.safeParse(this.formData);
+
+    if(!result.success){
+      console.warn('Invalid payload blocked', result.error)
+      return;
+    }
+    else{
+      console.log('The payload is being sent.');
+    }
+
+  }
+
+
+  
 }
